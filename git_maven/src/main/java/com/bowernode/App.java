8d8dc500078382073b5d5d5f76c2fe1d12518e99
gavin128 <919201515@qq.com>
2020-12-05 21:35:18 +0800

    添加test_git工程到git

---
+++ git_maven/src/main/java/com/bowernode/App.java
@@ -0,0 +1,13 @@
+package com.bowernode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
